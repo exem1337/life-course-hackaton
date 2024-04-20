cf428dbf56b63130bbf3accb41ff0e0c8e54a63d
KOMPIK\verem <mvv@vt2b.ru>
2024-04-20 17:59:38 +0500

    fix

--- src/components/profile/GalleryAllModal.vue
+++ src/components/profile/GalleryAllModal.vue
@@ -7,14 +7,63 @@
     content-class="flex flex-col max-w-xl mx-4 p-4 bg-white dark:bg-gray-900 border dark:border-gray-700 rounded-lg space-y-2"
     @cancel="$emit('confirm')"
   >
-    asdasd
+    <div class="gallery-container">
+      <q-scroll-area style="height: 610px; width: 60vw;">
+        <div class="gallery-container--list">
+          <q-avatar
+            v-for="image in images"
+            :key="image?.id"
+            class="gallery-container--list__image"
+            size="284px"
+            square
+            @click="openImage(image)"
+          >
+            <img
+              :src="image?.photo"
+              alt="Фото"
+            >
+          </q-avatar>
+        </div>
+      </q-scroll-area>
+    </div>
   </BaseModal>
 </template>
 
 <script setup lang="ts">
-
 import BaseModal from 'components/modals/base/BaseModal.vue';
+import { IGalleryItem } from 'src/models/profile/galleryImage.model';
+import { inject } from 'vue';
+import ModalManager from 'src/services/base/modalManager.service';
+import GalleryOneModal from 'components/profile/GalleryOneModal.vue';
+defineProps<{
+  images: IGalleryItem[];
+}>()
+const modalManager = inject<ModalManager>(ModalManager.getServiceName());
+function openImage(imageItem: IGalleryItem) {
+  // eslint-disable-next-line no-undef
+  modalManager?.openAsyncModal(GalleryOneModal, {
+    attrs: {
+      image: imageItem,
+    },
+  });
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.gallery-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  &--list {
+    display: flex;
+    flex-wrap: wrap;
+
+    &__image {
+      margin: 2px;
+      cursor: pointer;
+    }
+  }
+}
 </style>
---
+++ src/components/profile/GalleryOneModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <BaseModal
+    class="flex justify-center items-center"
+    overlay-transition="vfm-fade"
+    title="Фото"
+    hide-footer
+    content-class="flex flex-col max-w-xl mx-4 p-4 bg-white dark:bg-gray-900 border dark:border-gray-700 rounded-lg space-y-2"
+    @cancel="$emit('confirm')"
+  >
+    <div class="gallery-container">
+      <div class="gallery-container--list">
+        <img
+          class="gallery-container--list__image"
+          :src="image?.photo"
+          alt="Фото"
+        >
+      </div>
+    </div>
+  </BaseModal>
+</template>
+
+<script setup lang="ts">
+import BaseModal from 'components/modals/base/BaseModal.vue';
+import { IGalleryItem } from 'src/models/profile/galleryImage.model';
+defineProps<{
+  image: IGalleryItem;
+}>()
+</script>
+
+<style scoped lang="scss">
+.gallery-container {
+  max-width: 40vw;
+  height: 70vh;
+
+  &--list {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &__image {
+      margin: 2px;
+      max-height: 100%;
+      max-width: 100%;
+      object-fit: contain;
+    }
+  }
+}
+</style>
